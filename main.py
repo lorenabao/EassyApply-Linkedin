feceff03e9b9d2ec1eb808d0ccf096dad67e638d
lorenabao <lorena.bao@outlook.com>
2023-08-21 16:04:06 +0200

    Go to jobs page. Apply keyword and location to search jobs

--- main.py
+++ main.py
@@ -26,9 +26,25 @@ class EasyApplyLinkedin:
         login_password = self.driver.find_element_by_name("session_password")
         login_password.clear()
         login_password.send_keys(self.password)
+        login_password.send_keys(Keys.RETURN)
 
-        login_password.send_key(Keys.RETURN)
+    def job_search(self):
+        """This function goes to the 'Jobs' section and looks for all the jobs that matches the keywords"""
 
+        # Go to 'Jobs' section
+        self.driver.get("https://www.linkedin.com/jobs/search/?")
+
+        # Introduce keywords and location and hit enter
+        search_keyword = self.driver.find_element_by_xpath("//input[starts-with(@id, 'jobs-search-box-keyword')]")
+        search_keyword.clear()
+        search_keyword.send_keys(self.keywords)
+
+        search_location = self.driver.find_element_by_xpath("//input[starts-with(@id, 'jobs-search-box-location')]")
+        search_location.clear()
+        search_location.send_keys(self.location)
+
+        search_button = self.driver.find_element_by_css_selector(".jobs-search-box__submit-button")
+        search_button.click()
 
 
 if __name__ == "__main__":
@@ -37,3 +53,4 @@ if __name__ == "__main__":
 
     bot = EasyApplyLinkedin(data)
     bot.login_linkedin()
+    bot.job_search()
